3fa1eeb4cf906aa3bba3bcf53d0c537f87f25ea7
Ahmed mohamed taha <ahmed.wesal03@eng-st.cu.edu.eg>
2024-03-19 01:04:50 +0200

    adding signup latest update

--- app.py
+++ app.py
@@ -8,22 +8,20 @@ from mobility_asesement import *
 import serial
 from pyfirmata import Arduino, util
 
-
 # Initialize Arduino board
 board = Arduino('COM6')
 
 # Define the analog pin where your sensor is connected
+analog_pin = board.get_pin('a:1:i')
 analog_pin = board.get_pin('a:0:i')
 it = util.Iterator(board)
 it.start()
-sensor_value = analog_pin.read()
+
 def read_sensor():
     # Read sensor value
     sensor_value = analog_pin.read()
     if sensor_value is not None:
         scaled_value = int(sensor_value * 1023)
-    # Schedule the function to run again after 100 milliseconds
-    window.after(100, read_sensor)
     return scaled_value
 
 class MainWindow(QMainWindow):
@@ -31,13 +29,9 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.current_layout = None
         self.setupUi("signup")
-
-        # Open the serial port
-        #self.serialInst = serial.Serial("COM4", 9600)
-
         # Create a QTimer to read serial periodically
         self.timer = QTimer(self)
-        #self.timer.timeout.connect(self.read_serial)
+        self.timer.timeout.connect(self.read_serial)
         self.timer.start(100)  # Adjust the interval (milliseconds) as needed
 
     def setupUi(self, layout_name):
@@ -67,34 +61,11 @@ class MainWindow(QMainWindow):
         # Clear existing layout
         self.current_layout = None
         self.centralWidget().deleteLater()  # Clear existing widgets
-
         # Setup new layout
         self.setupUi(layout_name)
-
-    # def read_serial(self):
-    #     if self.serialInst.in_waiting:
-    #         # Read a line from the serial port
-    #         packet = self.serialInst.readline()
-    #         # Treat the line as the value itself
-    #         value = packet.decode('utf-8').strip()
-    #         # Convert the value to an integer
-    #         try:
-    #             value_int = int(value)
-    #             # Update your widget with the received value
-    #             self.current_layout.progressBar.setValue(value_int)
-    #         except ValueError:
-    #             print("Invalid value received from Arduino:", value)
-
-    # def closeEvent(self, event):
-    #     # Close the serial port when the window is closed
-    #     self.serialInst.close()
-    #     event.accept()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
-    # Example of switching layout after some time (3 seconds)
-    #QTimer.singleShot(3000, lambda: window.switch_layout("login"))
-
     sys.exit(app.exec_())
